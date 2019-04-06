51ff5ecf6632a995ba04e4af32a9899cb51b578c
Giulia Guidi <gl.guidi93@gmail.com>
2019-04-06 12:47:15 -0700

    temporary removing some simd to facilitate debbugging
    start making sense

--- src/cpu-avx2/logan-functions.h
+++ src/cpu-avx2/logan-functions.h
@@ -168,10 +168,6 @@ calcExtendedUpperDiag(unsigned short & upperDiag,
 //		short const &scoreDropOff)
 //{
 
-// The details of that allocation are implementation-defined, and it's undefined behavior to read from 
-// the member of the union that wasn't most recently written. Many compilers implement, as a non-standard 
-// language extension, the ability to read inactive members of a union.
-
 typedef union {
 	__m256i simd;
 	int16_t elem[16] = {-1};
@@ -189,6 +185,17 @@ void print_m256i_16(__m256i a) {
 			t.elem[ 8], t.elem[ 9], t.elem[10], t.elem[11],
 			t.elem[12], t.elem[13], t.elem[14], t.elem[15]
 			);
+}
+
+inline _m256i_16_t shiftAntiDiag2 (_m256i_16_t& data) {
+
+	_m256i_16_t m; 
+	for(int16_t i = 15; i > 0; i--) // data are saved in reversed order
+	{
+		m.elem[i] = data.elem[i-1];
+	}
+	m.elem[0] = data.elem[0];
+	return m;
 }
 
 int
@@ -260,8 +267,6 @@ extendSeedLGappedXDropRightAVX2(
 	int16_t antiDiag2size = 1;	 // init
 	int16_t antiDiag3size = 2;	 // init
 
-	int16_t antiDiagBest  = antiDiagNo * gapCost;	 // init
-
 	while (minCol < maxCol) // this diff cannot be greater than 16
 	{
 		// data must be aligned when loading to and storing to avoid severe performance penalties
@@ -282,46 +287,23 @@ extendSeedLGappedXDropRightAVX2(
 
 		//printf("antiDiag1size %d\n", antiDiag1size);
 		//printf("antiDiag2size %d\n", antiDiag2size);
-		//printf("antiDiag3size %d\n", antiDiag3size);
+		//printf("antiDiag3size %d offset3 %d minCol %d antiDiagNo %d\n", antiDiag3size, offset3, minCol, antiDiagNo);
 
 		offset1 = offset2;
 		offset2 = offset3;
 		offset3 = minCol - 1;
 
-		antiDiagBest  = antiDiagNo * gapCost;
+		int16_t antiDiagBest  = antiDiagNo * gapCost; // init
 
-		__m256i mask1 = _mm256_cmpgt_epi16 (_mm256_set1_epi16 (antiDiagBest), _mm256_set1_epi16 (best - scoreDropOff)); // if (antiDiagNo * gapCost > best - scoreDropOff) mask1 set to 1, otherwise 0
-		//print_m256i_16(mask1); //false == -1
-		__m256i mask2 = _mm256_cmpeq_epi16 (_mm256_set1_epi16 (offset3), _mm256_setzero_si256()); 	// if (offset3 == 0) mask2 set to 1, otherwise 0
-		//print_m256i_16(mask2); //false == -1
-		__m256i mask3 = _mm256_and_si256   (mask1, mask2); 	// if (antiDiagNo * gapCost > best - scoreDropOff) AND if (offset3 == 0) mask3 set to 1, otherwise 0
-		//print_m256i_16(mask3); //false == -1
-		__m256i mask4 = _mm256_cmpeq_epi16 (_mm256_set1_epi16 (antiDiagNo - maxCol), _mm256_setzero_si256()); 	// if (antiDiagNo - maxCol == 0) mask4 set to 1, otherwise 0
-		//print_m256i_16(mask4); //false == -1
-		__m256i mask5 = _mm256_and_si256   (mask1, mask4); 	// if (antiDiagNo * gapCost > best - scoreDropOff) AND if (antiDiagNo - maxCol == 0) mask5 set to 1, otherwise 0
-		//print_m256i_16(mask5); //false == -1
+		if (antiDiagNo * gapCost > best - scoreDropOff)
+		{
+			if (offset3 == 0) // init first column
+				antiDiag3.elem[0] = antiDiagNo * gapCost;
+			if (antiDiagNo - maxCol == 0) // init first row
+				antiDiag3.elem[maxCol - offset3] = antiDiagNo * gapCost;
+		}
 
-		_m256i_16_t mask6;
-		_m256i_16_t mask7;
-
-		mask6.elem[0] = 1;
-		mask7.elem[antiDiagNo - maxCol] = 1;
-
-		//print_m256i_16(mask6.simd);
-		//print_m256i_16(mask7.simd);
-
-		mask6.simd      = _mm256_mullo_epi16 (mask6.simd, mask3); // if mask3 == 0, mask6 == 0, otheriwise remain the same as declared
-		antiDiag3.simd  = _mm256_blendv_epi8 (antiDiag3.simd, _mm256_set1_epi16 (antiDiagBest), mask6.simd); // if mask6 == 0, antiDiag3 remain the same as before
-
-		mask7.simd      = _mm256_mullo_epi16 (mask7.simd, mask5); // if mask5 == 0, mask7 == 0, otheriwise remain the same as declared
-		antiDiag3.simd  = _mm256_blendv_epi8 (antiDiag3.simd, _mm256_set1_epi16 (antiDiagBest), mask7.simd); // if mask7 == 0, antiDiag3 remain the same as before
-
-		// check here what's going on here	
-		//print_m256i_16(mask6.simd);
-		//print_m256i_16(mask7.simd);
-		//print_m256i_16(antiDiag3.simd);
-
-		__m256i tmp;
+		_m256i_16_t tmp;
 
 		for (int16_t col = minCol; col < maxCol; col += 16)
 		{
@@ -333,40 +315,46 @@ extendSeedLGappedXDropRightAVX2(
 			int16_t dbPos = antiDiagNo - col - 1;
 
 			// calculate matrix entry (-> antiDiag3[col])
-			// fist horizonal max on antiDiag2
-			// int tmp = max(antiDiag2[i2-1], antiDiag2[i2]) + gapCost;
-			// equivalent to _mm256_slli_si256 with N = 16 		left shift
 			// TODO : double check after compilation and put into a separate function
-			tmp = _mm256_permute2x128_si256(antiDiag2.simd, antiDiag2.simd, _MM_SHUFFLE(0, 0, 2, 0));
-			//print_m256i_16(tmp);
+			_m256i_16_t test;
+			for(int16_t i = 0; i < 16; i++)
+				test.elem[i] = i;
+			//print_m256i_16(test.simd);
+			tmp = shiftAntiDiag2 (antiDiag2);
+			//print_m256i_16(tmp.simd);
+
 			//print_m256i_16(antiDiag2.simd);
 
-			// equivalent to _mm256_srli_si256 with N = 16 		right shift
-			// source : https://stackoverflow.com/questions/25248766/emulating-shifts-on-32-bytes-with-avx
-			// __m256 v1 = _mm256_permute2x128_si256(v0, v0, _MM_SHUFFLE(2, 0, 0, 1));
-			tmp = _mm256_max_epi16 (antiDiag2.simd, tmp);
-			tmp = _mm256_add_epi16 (tmp, _mm256_set1_epi16 (gapCost));
+			tmp.simd = _mm256_max_epi16 (antiDiag2.simd, tmp.simd);
+			//print_m256i_16(tmp.simd);
+			tmp.simd = _mm256_add_epi16 (tmp.simd, _mm256_set1_epi16 (gapCost));
+			//print_m256i_16(tmp.simd);
 
 			__m256i _m_query  = _mm256_loadu_si256 ((__m256i*)(query  + queryPos)); // load sixteen bases from querySeg
 			__m256i _m_target = _mm256_loadu_si256 ((__m256i*)(target + dbPos)); 	// load sixteen bases from targetSeg
 
-			// sequences make sense
-			//print_m256i_16(_m_query);
-			//print_m256i_16(_m_target);
-
 			// tmp = max(tmp, antiDiag1[i1 - 1] + score(scoringScheme, querySeg[queryPos], databaseSeg[dbPos]));
 			// here : score(scoringScheme, querySeg[queryPos], databaseSeg[dbPos])
 			__m256i tmpscore = _mm256_cmpeq_epi16 (_m_query, _m_target);
-			tmpscore = _mm256_blendv_epi8 (_mm256_set1_epi16 (scoreMatch(scoringScheme)), _mm256_set1_epi16 (scoreMismatch(scoringScheme)), tmpscore);
+			//print_m256i_16(tmpscore); // -1 where equal, 0 where different
+			tmpscore = _mm256_blendv_epi8 (_mm256_set1_epi16 (scoreMismatch(scoringScheme)), _mm256_set1_epi16 (scoreMatch(scoringScheme)), tmpscore);
+			//print_m256i_16(tmpscore);
 			// here : add tmpscore to antiDiag1 	
 			tmpscore = _mm256_add_epi16 (tmpscore, antiDiag1.simd);
-			tmp = _mm256_max_epi16 (tmp, tmpscore);
+			tmp.simd = _mm256_max_epi16 (tmp.simd, tmpscore);
 
 			//if (tmp < best - scoreDropOff)
-			__m256i mask8 = _mm256_cmpgt_epi16 (_mm256_set1_epi16 (best - scoreDropOff), tmp);
-			antiDiag3.simd = _mm256_blendv_epi8 (_mm256_set1_epi16 (undefined), tmp, mask8);
-			// never updated!
-			//print_m256i_16(antiDiag3.simd);
+			//{
+			//	antiDiag3[i3] = undefined;
+			//}
+			//else
+			//{
+			//	antiDiag3[i3] = tmp;
+			//	//antiDiagBest = max(antiDiagBest, tmp);
+			//}
+			__m256i mask8 = _mm256_cmpgt_epi16 (_mm256_set1_epi16 (best - scoreDropOff), tmp.simd);
+			antiDiag3.simd = _mm256_blendv_epi8 (tmp.simd, _mm256_set1_epi16 (undefined), mask8);
+			print_m256i_16(antiDiag3.simd);
 
 			// TODO: skipping control here double check 	
 			// if true, this should contain antiDiagBest it shouldn't harm
@@ -374,7 +362,7 @@ extendSeedLGappedXDropRightAVX2(
 		}
 
 		// antiDiagBest = *max_element(antiDiag3.begin(), antiDiag3.end());
-		antiDiagBest = _mm256_extract_epi16 (_mm256_max_epi16 (_mm256_set1_epi16 (antiDiagBest), tmp), 0);
+		antiDiagBest = _mm256_extract_epi16 (_mm256_max_epi16 (_mm256_set1_epi16 (antiDiagBest), tmp.simd), 0);
 		// best = (best > antiDiagBest) ? best : antiDiagBest;
 		// ones where best is greater, otherwise zeros
 		__m256i mask9 = _mm256_cmpgt_epi16 (_mm256_set1_epi16 (best), _mm256_set1_epi16 (antiDiagBest));
