e58ecdd0214c23e093035d12c313270c528b7294
Giulia Guidi <gl.guidi93@gmail.com>
2019-04-23 14:05:27 -0700

    benchmark draft ok
    there's something fishy

--- src/cpu-avx2/simulator.cpp
+++ src/cpu-avx2/simulator.cpp
@@ -13,18 +13,24 @@
 #include<inttypes.h>
 #include<assert.h>
 #include<iterator>
-#include"logan-v2.cpp"
 #include <immintrin.h>
+#include <seqan/sequence.h>
+#include <seqan/align.h>
+#include <seqan/seeds.h>
+#include <seqan/score.h>
+#include <seqan/modifier.h>
+#include"logan-v2.cpp"
 
 //======================================================================================
 // READ SIMULATOR
 //======================================================================================
 
-#define LEN (10000)	// read length (this is going to be a distribution of length in
+#define LEN (32)		// read length (this is going to be a distribution of length in
 						// the adaptive version)
-#define MAT	( 1)
-#define MIS	(-1)
-#define GAP	(-1)
+#define MAT	( 1)		// match score
+#define MIS	(-1)		// mismatch score
+#define GAP	(-1)		// gap score
+#define XDROP (1000)	// so high so it won't be triggered in SeqAn
 
 void 
 readSimulator (std::string& readh, std::string& readv)
@@ -34,8 +40,9 @@ readSimulator (std::string& readh, std::string& readv)
 	for (int i = 0; i < LEN; i++)
 	{
 		// two identical sequences
-		readh = readh + bases[rand() % 4]; 
-		readv = readv + bases[rand() % 4];
+		int test = rand();
+		readh = readh + bases[test % 4]; 
+		readv = readv + bases[test % 4];
 	}
 }
 
@@ -46,14 +53,28 @@ readSimulator (std::string& readh, std::string& readv)
 int main(int argc, char const *argv[])
 {
 	std::string targetSeg, querySeg;
-	ScoringSchemeL scoringScheme(MAT, MIS, GAP);
 
+	// Simulate pair of read
+	readSimulator(targetSeg, querySeg);
+	std::cout << std::endl;
+	std::cout << targetSeg << std::endl;
+	std::cout << std::endl;
+	std::cout << querySeg  << std::endl;
+	std::cout << std::endl;
+
+	// Logan
+	ScoringSchemeL scoringSchemeLogan(MAT, MIS, GAP);
 	// 1st prototype without seed and x-drop termination (not adaptive band so sequences 
 	// have same length)
-	// sim a pair of read
-	readSimulator(targetSeg, querySeg);
-	// calling Logan
-	LoganAVX2(targetSeg, querySeg, scoringScheme);
+	LoganAVX2(targetSeg, querySeg, scoringSchemeLogan);
+
+	// SeqAn
+	seqan::Score<int, seqan::Simple> scoringSchemeSeqAn(MAT, MIS, GAP);
+	seqan::Seed<seqan::Simple> seed(0, 0, 0);
+	int score = seqan::extendSeed(seed, targetSeg, querySeg, seqan::EXTEND_RIGHT, 
+		scoringSchemeSeqAn, XDROP, seqan::GappedXDrop(), 0);
+
+	std::cout << "" << score << std::endl;
 
 	return 0;
 }
