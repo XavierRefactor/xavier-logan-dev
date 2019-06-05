f65c4d4414404e9d9f9974219d7808b4a0913dfe
Ed Younis <edyounis123@gmail.com>
2019-06-05 10:58:34 -0700

    Untested Affine Gap

--- src/simd/logan_xa.h
+++ src/simd/logan_xa.h
@@ -46,10 +46,13 @@ LoganOneDirection
 	short matchCost    = scoreMatch(scoringScheme   );
 	short mismatchCost = scoreMismatch(scoringScheme);
 	short gapCost      = scoreGap(scoringScheme     );
+	short gapOpening   = -1; // scoreGapOpening()
 
 	vector_t vmatchCost    = set1_func (matchCost   );
 	vector_t vmismatchCost = set1_func (mismatchCost);
 	vector_t vgapCost      = set1_func (gapCost     );
+	vector_t vzeros        = _mm256_setzero_si256();
+	vector_t vgapopening   = set1_func (gapOpening  );
 
 	//======================================================================================
 	// PHASE I (initial values load using dynamic programming)
@@ -60,17 +63,32 @@ LoganOneDirection
 #endif
 	// we need one more space for the off-grid values and one more space for antiDiag2
 	short phase1_data[LOGICALWIDTH + 2][LOGICALWIDTH + 2];
+	short phase1_gaphistup[ LOGICALWIDTH + 2 ][ LOGICALWIDTH + 2 ];
+	short phase1_gaphistleft[ LOGICALWIDTH + 2 ][ LOGICALWIDTH + 2 ];
 
 	// phase1_data initialization
 	phase1_data[0][0] = 0;
 	for (int i = 1; i < LOGICALWIDTH + 2; i++)
 	{
-		phase1_data[0][i] = -i;
-		phase1_data[i][0] = -i;
+		phase1_data[0][i] = -i + gapOpening;
+		phase1_data[i][0] = -i + gapOpening;
+	}
+
+	phase1_gaphistleft[0][0] = gapOpening;
+	phase1_gaphistup[0][0]   = gapOpening;
+
+	for (int i = 1; i < LOGICALWIDTH + 2; i++)
+	{
+		phase1_gaphistleft[0][i] = 0;
+		phase1_gaphistup[0][i]   = gapOpening;
+
+		phase1_gaphistleft[i][0] = gapOpening;
+		phase1_gaphistup[i][0]   = 0;
 	}
 
 	// dynamic programming loop to fill phase1_data[][]
 	for(int i = 1; i < LOGICALWIDTH + 2; i++)
+	{
 		for(int j = 1; j < LOGICALWIDTH + 2; j++)
 		{
 			short onef = phase1_data[i-1][j-1];
@@ -79,9 +97,29 @@ LoganOneDirection
 			else
 				onef += mismatchCost;
 
-			short twof = std::max(phase1_data[i-1][j], phase1_data[i][j-1]) + gapCost;
+			short twou = phase1_data[i-1][j] + gapCost + phase1_gaphistup[i-1][j];
+			short twol = phase1_data[i][j-1] + gapCost + phase1_gaphistleft[i][j-1];
+			short twof = std::max( twou, twol );
+
 			phase1_data[i][j] = std::max(onef, twof);
+
+			if ( phase1_data[i][j] == onef )
+			{
+				phase1_gaphistup[i][j]   = gapOpening;
+				phase1_gaphistleft[i][j] = gapOpening;
+			}
+			else if ( phase1_data[i][j] == twou )
+			{
+				phase1_gaphistup[i][j]   = 0;
+				phase1_gaphistleft[i][j] = gapOpening;
+			}
+			else
+			{
+				phase1_gaphistup[i][j]   = gapOpening;
+				phase1_gaphistleft[i][j] = 0;
+			}
 		}
+	}
 
 #ifdef DEBUG
 	// print phase1_data[][]
@@ -96,6 +134,9 @@ LoganOneDirection
 	vector_union_t antiDiag1; 	// 16 (vector width) 16-bit integers
 	vector_union_t antiDiag2; 	// 16 (vector width) 16-bit integers
 	vector_union_t antiDiag3; 	// 16 (vector width) 16-bit integers
+
+	vector_union_t gaphist_up;
+	vector_union_t gaphist_left;
 
 	vector_union_t vqueryh;
 	vector_union_t vqueryv;
@@ -126,8 +167,17 @@ LoganOneDirection
 		antiDiag2.elem[i] = phase1_data[i + 1][LOGICALWIDTH - i + 1];
 	antiDiag2.elem[0] = NINF;
 
-	// initialize antiDia3 to -inf
+	// initialize antiDiag3 to -inf
 	antiDiag3.simd = set1_func (NINF);
+
+	// initialize gaphist
+	for (int i = 1; i <= LOGICALWIDTH; ++i)
+		gaphist_left.elem[i] = phase1_gaphistleft[i + 1][LOGICALWIDTH - i + 1];
+	//gaphist_left.elem[0] = NINF;
+
+	for (int i = 1; i <= LOGICALWIDTH; ++i)
+		gaphist_up.elem[i] = phase1_gaphistup[i + 1][LOGICALWIDTH - i + 1];
+	//gaphist_up.elem[0] = NINF;
 
 	//======================================================================================
 	// PHASE II (core vectorized computation)
@@ -167,30 +217,37 @@ LoganOneDirection
 		print_vector_d(antiDiag1F);
 	#endif
 
-		// antiDiag2S (shift)
-		vector_union_t antiDiag2S = leftShift (antiDiag2);
+		// antiDiag2U (Up)
+		vector_union_t antiDiag2U = leftShift (antiDiag2);
+		antiDiag2U.simd = add_func (antiDiag2U.simd, vgapCost);
+		antiDiag2U.simd = add_func (antiDiag2U.simd, gaphist_up);
 	#ifdef DEBUG
-		printf("antiDiag2S: ");
-		print_vector_d(antiDiag2S.simd);
+		printf("antiDiag2U: ");
+		print_vector_d(antiDiag2U.simd);
 	#endif
+
+	// antiDiag2L (Left)
+		vector_union_t antiDiag2L = antiDiag2;
+		antiDiag2L.elem[VECTORWIDTH - 1] = NINF;
+		antiDiag2L.simd = add_func (antiDiag2L.simd, vgapCost);
+		antiDiag2L.simd = add_func (antiDiag2L.simd, gaphist_left);
+	#ifdef DEBUG
+		printf("antiDiag2L: ");
+		print_vector_d(antiDiag2L.simd);
+	#endif
+
 		// antiDiag2M (pairwise max)
-		vector_t antiDiag2M = max_func (antiDiag2S.simd, antiDiag2.simd);
+		vector_t antiDiag2M = max_func (antiDiag2L.simd, antiDiag2U.simd);
 	#ifdef DEBUG
 		printf("antiDiag2M: ");
 		print_vector_d(antiDiag2M);
-	#endif
-		// antiDiag2F (final)
-		vector_t antiDiag2F = add_func (antiDiag2M, vgapCost);
-	#ifdef DEBUG
-		printf("antiDiag2F: ");
-		print_vector_d(antiDiag2F);
 	#endif
 	#ifdef DEBUG
 		printf("antiDiag2: ");
 		print_vector_d(antiDiag2.simd);
 	#endif
 		// Compute antiDiag3
-		antiDiag3.simd = max_func (antiDiag1F, antiDiag2F);
+		antiDiag3.simd = max_func (antiDiag1F, antiDiag2M);
 		// we need to have always antiDiag3 left-aligned
 		antiDiag3.elem[LOGICALWIDTH] = NINF;
 	#ifdef DEBUG
@@ -198,7 +255,16 @@ LoganOneDirection
 		print_vector_d(antiDiag3.simd);
 	#endif
 
+		// Gap History Computation
+		vector_t 2gap = cmpeq_func (antiDiag2M, antiDiag2U.simd);
+		gaphist_up.simd = blendv_func (vzeros, vgapopening, 2gap); // TODO: Check values
+		gaphist_left.simd = blendv_func (vgapopening, vzeros, 2gap); // TODO: Check values
+		vector_t 3gap = cmpeq_func (antiDiag3.simd, antiDiag1F);
+		gaphist_up.simd = blendv_func (vgapopening, gaphist_up.simd, 3gap); // TODO: Check values
+		gaphist_left.simd = blendv_func (vgapopening, gaphist_left.simd, 3gap); // TODO: Check values
+
 		// TODO: x-drop termination
+		// Note: Don't need to check x drop every time
 		antiDiagBest = *std::max_element(antiDiag3.elem, antiDiag3.elem + VECTORWIDTH);
 		if(antiDiagBest < best - scoreDropOff)
 		{
@@ -250,11 +316,12 @@ LoganOneDirection
 	while(hoffset < hlength || voffset < vlength)
 	{
 
-	#ifdef DEBUG
-		printf("\n");
-		print_vector_c(vqueryh.simd);
-		print_vector_c(vqueryv.simd);
-	#endif
+#ifdef DEBUG
+	printf("\n");
+	print_vector_c(vqueryh.simd);
+	print_vector_c(vqueryv.simd);
+#endif
+
 		// antiDiagBest initialization
 		antiDiagNo++;
 		antiDiagBest = antiDiagNo * gapCost;
@@ -272,36 +339,51 @@ LoganOneDirection
 		print_vector_d(antiDiag1F);
 	#endif
 
-		// antiDiag2S (shift)
-		vector_union_t antiDiag2S = leftShift (antiDiag2);
+		// antiDiag2U (Up)
+		vector_union_t antiDiag2U = leftShift (antiDiag2);
+		antiDiag2U.simd = add_func (antiDiag2U.simd, vgapCost);
+		antiDiag2U.simd = add_func (antiDiag2U.simd, gaphist_up);
 	#ifdef DEBUG
-		printf("antiDiag2S: ");
-		print_vector_d(antiDiag2S.simd);
+		printf("antiDiag2U: ");
+		print_vector_d(antiDiag2U.simd);
 	#endif
+
+	// antiDiag2L (Left)
+		vector_union_t antiDiag2L = antiDiag2;
+		antiDiag2L.elem[VECTORWIDTH - 1] = NINF;
+		antiDiag2L.simd = add_func (antiDiag2L.simd, vgapCost);
+		antiDiag2L.simd = add_func (antiDiag2L.simd, gaphist_left);
+	#ifdef DEBUG
+		printf("antiDiag2L: ");
+		print_vector_d(antiDiag2L.simd);
+	#endif
+
 		// antiDiag2M (pairwise max)
-		vector_t antiDiag2M = max_func (antiDiag2S.simd, antiDiag2.simd);
+		vector_t antiDiag2M = max_func (antiDiag2L.simd, antiDiag2U.simd);
 	#ifdef DEBUG
 		printf("antiDiag2M: ");
 		print_vector_d(antiDiag2M);
-	#endif
-		// antiDiag2F (final)
-		vector_t antiDiag2F = add_func (antiDiag2M, vgapCost);
-	#ifdef DEBUG
-		printf("antiDiag2F: ");
-		print_vector_d(antiDiag2F);
 	#endif
 	#ifdef DEBUG
 		printf("antiDiag2: ");
 		print_vector_d(antiDiag2.simd);
 	#endif
 		// Compute antiDiag3
-		antiDiag3.simd = max_func (antiDiag1F, antiDiag2F);
+		antiDiag3.simd = max_func (antiDiag1F, antiDiag2M);
 		// we need to have always antiDiag3 left-aligned
 		antiDiag3.elem[LOGICALWIDTH] = NINF;
 	#ifdef DEBUG
 		printf("antiDiag3: ");
 		print_vector_d(antiDiag3.simd);
 	#endif
+
+		// Gap History Computation
+		vector_t 2gap = cmpeq_func (antiDiag2M, antiDiag2U.simd);
+		gaphist_up.simd = blendv_func (vzeros, vgapopening, 2gap); // TODO: Check values
+		gaphist_left.simd = blendv_func (vgapopening, vzeros, 2gap); // TODO: Check values
+		vector_t 3gap = cmpeq_func (antiDiag3.simd, antiDiag1F);
+		gaphist_up.simd = blendv_func (vgapopening, gaphist_up.simd, 3gap); // TODO: Check values
+		gaphist_left.simd = blendv_func (vgapopening, gaphist_left.simd, 3gap); // TODO: Check values
 
 		// x-drop termination
 		antiDiagBest = *std::max_element(antiDiag3.elem, antiDiag3.elem + VECTORWIDTH);
@@ -341,6 +423,17 @@ LoganOneDirection
 #endif
 	for (int i = 0; i < (LOGICALWIDTH - 3); i++)
 	{
+
+#ifdef DEBUG
+	printf("\n");
+	print_vector_c(vqueryh.simd);
+	print_vector_c(vqueryv.simd);
+#endif
+
+		// antiDiagBest initialization
+		antiDiagNo++;
+		antiDiagBest = antiDiagNo * gapCost;
+
 		// antiDiag1F (final)
 		// POST-IT: -1 for a match and 0 for a mismatch
 		vector_t m = cmpeq_func (vqueryh.simd, vqueryv.simd);
@@ -348,44 +441,57 @@ LoganOneDirection
 		vector_t antiDiag1F = add_func (m, antiDiag1.simd);
 
 	#ifdef DEBUG
-		printf("\n");
 		printf("antiDiag1: ");
 		print_vector_d(antiDiag1.simd);
 		printf("antiDiag1F: ");
 		print_vector_d(antiDiag1F);
 	#endif
 
-		// antiDiag2S (shift)
-		vector_union_t antiDiag2S = leftShift (antiDiag2);
+		// antiDiag2U (Up)
+		vector_union_t antiDiag2U = leftShift (antiDiag2);
+		antiDiag2U.simd = add_func (antiDiag2U.simd, vgapCost);
+		antiDiag2U.simd = add_func (antiDiag2U.simd, gaphist_up);
 	#ifdef DEBUG
-		printf("antiDiag2S: ");
-		print_vector_d(antiDiag2S.simd);
+		printf("antiDiag2U: ");
+		print_vector_d(antiDiag2U.simd);
 	#endif
+
+	// antiDiag2L (Left)
+		vector_union_t antiDiag2L = antiDiag2;
+		antiDiag2L.elem[VECTORWIDTH - 1] = NINF;
+		antiDiag2L.simd = add_func (antiDiag2L.simd, vgapCost);
+		antiDiag2L.simd = add_func (antiDiag2L.simd, gaphist_left);
+	#ifdef DEBUG
+		printf("antiDiag2L: ");
+		print_vector_d(antiDiag2L.simd);
+	#endif
+
 		// antiDiag2M (pairwise max)
-		vector_t antiDiag2M = max_func (antiDiag2S.simd, antiDiag2.simd);
+		vector_t antiDiag2M = max_func (antiDiag2L.simd, antiDiag2U.simd);
 	#ifdef DEBUG
 		printf("antiDiag2M: ");
 		print_vector_d(antiDiag2M);
 	#endif
-
-		// antiDiag2F (final)
-		vector_t antiDiag2F = add_func (antiDiag2M, vgapCost);
-	#ifdef DEBUG
-		printf("antiDiag2F: ");
-		print_vector_d(antiDiag2F);
-	#endif
-		// Compute antiDiag3
-		antiDiag3.simd = max_func (antiDiag1F, antiDiag2F);
-		// we need to have always antiDiag3 left-aligned
-		antiDiag3.elem[LOGICALWIDTH] = NINF;
 	#ifdef DEBUG
 		printf("antiDiag2: ");
 		print_vector_d(antiDiag2.simd);
 	#endif
+		// Compute antiDiag3
+		antiDiag3.simd = max_func (antiDiag1F, antiDiag2M);
+		// we need to have always antiDiag3 left-aligned
+		antiDiag3.elem[LOGICALWIDTH] = NINF;
 	#ifdef DEBUG
 		printf("antiDiag3: ");
 		print_vector_d(antiDiag3.simd);
 	#endif
+
+		// Gap History Computation
+		vector_t 2gap = cmpeq_func (antiDiag2M, antiDiag2U.simd);
+		gaphist_up.simd = blendv_func (vzeros, vgapopening, 2gap); // TODO: Check values
+		gaphist_left.simd = blendv_func (vgapopening, vzeros, 2gap); // TODO: Check values
+		vector_t 3gap = cmpeq_func (antiDiag3.simd, antiDiag1F);
+		gaphist_up.simd = blendv_func (vgapopening, gaphist_up.simd, 3gap); // TODO: Check values
+		gaphist_left.simd = blendv_func (vgapopening, gaphist_left.simd, 3gap); // TODO: Check values
 
 		// x-drop termination
 		antiDiagBest = *std::max_element(antiDiag3.elem, antiDiag3.elem + VECTORWIDTH);
